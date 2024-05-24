044ba5abe090e5633eb53a638ad67e0b863a44c7
gulsevim-blbl <gulsevimblbl@gmail.com>
2024-05-24 21:47:51 +0300

    dataCenterList add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Map from "./Map";
+import DataCentersList from "./DataCentersList";
 
 function App() {
   return (
     <div className="App">
       <Map/>
+      <DataCentersList/>
     </div>
   );
 }
---
+++ src/DataCentersList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import data from './data/data_centers.json';
+import "./datacenterslist.css";
+
+// DataCenter arayüzü
+interface DataCenter {
+  id: string;
+  Name: string;
+  Latitude: number;
+  Longitude: number;
+  Provider: string;
+}
+
+const DataCentersList = () => {
+  return (
+    <div className="data-center-list">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Provider</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data).map((providerKey) => {
+            const provider = providerKey as keyof typeof data;
+            return data[provider].map((center: DataCenter) => (
+              <tr key={center.id}>
+                <td>{center.Name}</td>
+                <td>{center.Latitude}</td>
+                <td>{center.Longitude}</td>
+                <td>{center.Provider}</td>
+              </tr>
+            ));
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default DataCentersList;
